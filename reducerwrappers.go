75182ae97bc41b56d64527ffb6ebe04917404f13
Zoltán Domahidi <domahidizoltan@gmail.com>
2025-02-16 21:36:40 +0100

    add fold, reduce, sum, max, min and avg reducers

--- reducerwrappers.go
+++ reducerwrappers.go
@@ -2,6 +2,7 @@ package steps
 
 import (
 	"fmt"
+	"math"
 	"reflect"
 )
 
@@ -31,4 +32,146 @@ func GroupBy[IN0 any, OUT0 comparable, OUT1 any](fn func(in IN0) (OUT0, OUT1, er
 			return ArgTypes{reflect.TypeFor[OUT0](), reflect.TypeFor[OUT1]()}, nil
 		},
 	}
+}
+
+func Fold[IN0 any](initValue IN0, reduceFn func(in1, in2 IN0) (IN0, error)) ReducerWrapper {
+	prevValue := initValue
+	return ReducerWrapper{
+		Name: "Fold",
+		ReducerFn: func(in StepInput) StepOutput {
+			currentVal := in.Args[0].(IN0)
+			nextValue, err := reduceFn(prevValue, currentVal)
+			prevValue = nextValue
+			return StepOutput{
+				Args:    Args{nextValue},
+				ArgsLen: 1,
+				Error:   err,
+			}
+		},
+		Validate: func(prevStepOut ArgTypes) (ArgTypes, error) {
+			inArgTypes := ArgTypes{reflect.TypeFor[IN0]()}
+			for i := range maxArgs {
+				if i == 0 && prevStepOut[0] == reflect.TypeFor[SkipFirstArgValidation]() {
+					continue
+				}
+				if prevStepOut[i] != inArgTypes[i] {
+					return ArgTypes{}, fmt.Errorf("%w [%s!=%s:%d]", ErrIncompatibleInArgType, prevStepOut[i].String(), inArgTypes[i].String(), i+1)
+				}
+			}
+			return ArgTypes{reflect.TypeFor[IN0]()}, nil
+		},
+	}
+}
+
+func Reduce[IN0 any](fn func(in1, in2 IN0) (IN0, error)) ReducerWrapper {
+	var initValue IN0
+	fold := Fold(initValue, fn)
+	fold.Name = "Reduce"
+	return fold
+}
+
+type number interface {
+	int | int8 | int16 | int32 | int64 |
+		uint | uint8 | uint16 | uint32 | uint64 |
+		float32 | float64
+}
+
+func Sum[IN0 number]() ReducerWrapper {
+	sumFn := Reduce(func(in1, in2 IN0) (IN0, error) {
+		return in1 + in2, nil
+	})
+	sumFn.Name = "Sum"
+	return sumFn
+}
+
+func Max[IN0 number]() ReducerWrapper {
+	var minValue any
+	switch reflect.TypeFor[IN0]().Kind() {
+	case reflect.Int:
+		minValue = int(math.MinInt)
+	case reflect.Int8:
+		minValue = int8(math.MinInt8)
+	case reflect.Int16:
+		minValue = int16(math.MinInt16)
+	case reflect.Int32:
+		minValue = int32(math.MinInt32)
+	case reflect.Int64:
+		minValue = int64(math.MinInt64)
+	case reflect.Uint:
+		minValue = uint(0)
+	case reflect.Uint8:
+		minValue = uint8(0)
+	case reflect.Uint16:
+		minValue = uint16(0)
+	case reflect.Uint32:
+		minValue = uint32(0)
+	case reflect.Uint64:
+		minValue = uint64(0)
+	case reflect.Float32:
+		minValue = -float32(math.MaxFloat32)
+	case reflect.Float64:
+		minValue = -float64(math.MaxFloat64)
+	}
+	maxFn := Fold(minValue.(IN0), func(in1, in2 IN0) (IN0, error) {
+		if in1 > in2 {
+			return in1, nil
+		}
+		return in2, nil
+	})
+	maxFn.Name = "Max"
+	return maxFn
+}
+
+func Min[IN0 number]() ReducerWrapper {
+	var maxValue any
+	switch reflect.TypeFor[IN0]().Kind() {
+	case reflect.Int:
+		maxValue = int(math.MaxInt)
+	case reflect.Int8:
+		maxValue = int8(math.MaxInt8)
+	case reflect.Int16:
+		maxValue = int16(math.MaxInt16)
+	case reflect.Int32:
+		maxValue = int32(math.MaxInt32)
+	case reflect.Int64:
+		maxValue = int64(math.MaxInt64)
+	case reflect.Uint:
+		maxValue = uint(math.MaxUint)
+	case reflect.Uint8:
+		maxValue = uint8(math.MaxUint8)
+	case reflect.Uint16:
+		maxValue = uint16(math.MaxUint16)
+	case reflect.Uint32:
+		maxValue = uint32(math.MaxUint32)
+	case reflect.Uint64:
+		maxValue = uint64(math.MaxUint64)
+	case reflect.Float32:
+		maxValue = float32(math.MaxFloat32)
+	case reflect.Float64:
+		maxValue = float64(math.MaxFloat64)
+	}
+
+	minFn := Fold(maxValue.(IN0), func(in1, in2 IN0) (IN0, error) {
+		if in1 < in2 {
+			return in1, nil
+		}
+		return in2, nil
+	})
+	minFn.Name = "Min"
+	return minFn
+}
+
+func Avg() ReducerWrapper {
+	var counter, avg, sum float64
+
+	avgFn := Reduce(func(in1, in2 float64) (float64, error) {
+		in1 = in1 * counter
+		sum = in1 + in2
+		counter++
+		avg = float64(sum) / counter
+
+		return avg, nil
+	})
+	avgFn.Name = "Avg"
+	return avgFn
 }
--- reducerwrappers_test.go
+++ reducerwrappers_test.go
@@ -2,6 +2,7 @@ package steps
 
 import (
 	"errors"
+	"math"
 	"reflect"
 	"testing"
 
@@ -73,4 +74,175 @@ func TestGroupBy_Validate(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestFold_Success(t *testing.T) {
+	actual := Transform[int]([]int{1, 2, 3, 4}).
+		With(Aggregate(
+			Fold(-4, func(in1, in2 int) (int, error) {
+				return in1 + in2, nil
+			}))).
+		AsSlice(expectsError(t, false))
+
+	assert.Equal(t, []any{6}, actual)
+}
+
+func TestFold_Failure(t *testing.T) {
+	actual := Transform[int]([]int{1, 2}).
+		With(Aggregate(
+			Fold(-4, func(in1, in2 int) (int, error) {
+				var err error
+				if in2 == 2 {
+					err = errors.New("fold error")
+				}
+				return in1 + in2, err
+			}))).
+		AsSlice(expectsError(t, true))
+
+	assert.Nil(t, actual)
+}
+
+func TestFold_Validate(t *testing.T) {
+	for _, sc := range []struct {
+		name         string
+		prevStepOut  ArgTypes
+		expectedOut  ArgTypes
+		expectsError bool
+	}{
+		{
+			name:        "matching_prev_step_out_type",
+			prevStepOut: ArgTypes{reflect.TypeFor[int]()},
+			expectedOut: ArgTypes{reflect.TypeFor[int]()},
+		}, {
+			name:         "different_prev_step_out_type",
+			prevStepOut:  ArgTypes{reflect.TypeFor[string]()},
+			expectsError: true,
+		}, {
+			name:        "skip_type_check_when_first_step",
+			prevStepOut: ArgTypes{reflect.TypeFor[SkipFirstArgValidation]()},
+			expectedOut: ArgTypes{reflect.TypeFor[int]()},
+		},
+	} {
+		t.Run(sc.name, func(t *testing.T) {
+			actualOut, actualErr := Fold(-4, func(in1, in2 int) (int, error) {
+				return 0, nil
+			}).Validate(sc.prevStepOut)
+
+			assert.Equal(t, sc.expectedOut, actualOut)
+			if sc.expectsError {
+				assert.ErrorIs(t, actualErr, ErrIncompatibleInArgType)
+				assert.ErrorContains(t, actualErr, "[string!=int:1]")
+			} else {
+				assert.NoError(t, actualErr)
+			}
+		})
+	}
+}
+
+// the following reducers are re-using Fold, so only the happy paths are tested here
+func TestReduce_Success(t *testing.T) {
+	actual := Transform[int]([]int{1, 2, 3, 4}).
+		With(Aggregate(
+			Reduce(func(in1, in2 int) (int, error) {
+				return in1 + in2, nil
+			}))).
+		AsSlice(expectsError(t, false))
+
+	assert.Equal(t, []any{10}, actual)
+}
+
+func TestSum_Success(t *testing.T) {
+	actual := Transform[uint8]([]uint8{1, 2, 3, 4}).
+		With(Aggregate(
+			Sum[uint8](),
+		)).
+		AsSlice(expectsError(t, false))
+
+	assert.Equal(t, []any{uint8(10)}, actual)
+}
+
+const float64EqualityThreshold = 1e-9
+
+func TestMax_Success(t *testing.T) {
+	testMax(t, []int{-1, -2, -3}, int(-1))
+	testMax(t, []int8{-1, -2, -3}, int8(-1))
+	testMax(t, []int16{-1, -2, -3}, int16(-1))
+	testMax(t, []int32{-1, -2, -3}, int32(-1))
+	testMax(t, []int64{-1, -2, -3}, int64(-1))
+
+	testMax(t, []uint{1, 2, 3}, uint(3))
+	testMax(t, []uint8{1, 2, 3}, uint8(3))
+	testMax(t, []uint16{1, 2, 3}, uint16(3))
+	testMax(t, []uint32{1, 2, 3}, uint32(3))
+	testMax(t, []uint64{1, 2, 3}, uint64(3))
+
+	testMax(t, []float32{-1, -2, -3}, float32(-1))
+	testMax(t, []float64{-1, -2, -3}, float64(-1))
+}
+
+func testMax[T number](t *testing.T, input []T, expected T) {
+	t.Helper()
+	actual := Transform[T](input).
+		With(Aggregate(
+			Max[T](),
+		)).
+		AsSlice(expectsError(t, false))
+
+	switch reflect.TypeFor[T]().Kind() {
+	case reflect.Float32:
+		a := actual[0].(float32)
+		assert.Less(t, math.Abs(float64(expected)-float64(a)), float64EqualityThreshold)
+	case reflect.Float64:
+		a := actual[0].(float64)
+		assert.Less(t, math.Abs(float64(expected)-float64(a)), float64EqualityThreshold)
+	default:
+		assert.Equal(t, []any{expected}, actual)
+	}
+}
+
+func TestMin_Success(t *testing.T) {
+	testMin(t, []int{-1, -2, -3}, int(-3))
+	testMin(t, []int8{-1, -2, -3}, int8(-3))
+	testMin(t, []int16{-1, -2, -3}, int16(-3))
+	testMin(t, []int32{-1, -2, -3}, int32(-3))
+	testMin(t, []int64{-1, -2, -3}, int64(-3))
+
+	testMin(t, []uint{1, 2, 3}, uint(1))
+	testMin(t, []uint8{1, 2, 3}, uint8(1))
+	testMin(t, []uint16{1, 2, 3}, uint16(1))
+	testMin(t, []uint32{1, 2, 3}, uint32(1))
+	testMin(t, []uint64{1, 2, 3}, uint64(1))
+
+	testMin(t, []float32{-1, -2, -3}, float32(-3))
+	testMin(t, []float64{-1, -2, -3}, float64(-3))
+}
+
+func testMin[T number](t *testing.T, input []T, expected T) {
+	t.Helper()
+	actual := Transform[T](input).
+		With(Aggregate(
+			Min[T](),
+		)).
+		AsSlice(expectsError(t, false))
+
+	switch reflect.TypeFor[T]().Kind() {
+	case reflect.Float32:
+		a := actual[0].(float32)
+		assert.Less(t, math.Abs(float64(expected)-float64(a)), float64EqualityThreshold)
+	case reflect.Float64:
+		a := actual[0].(float64)
+		assert.Less(t, math.Abs(float64(expected)-float64(a)), float64EqualityThreshold)
+	default:
+		assert.Equal(t, []any{expected}, actual)
+	}
+}
+
+func TestAvg_Success(t *testing.T) {
+	actual := Transform[float64]([]float64{-1.25, 1.33, -0.77}).
+		With(Aggregate(
+			Avg(),
+		)).
+		AsSlice(expectsError(t, false))
+
+	assert.Less(t, math.Abs(float64(-0.23)-actual[0].(float64)), float64EqualityThreshold)
 }
